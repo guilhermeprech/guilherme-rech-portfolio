fc5c08209d1a4e138ab4282f13e7eee87126e05e
guilhermeprech <guilhermepasqualirech@gmail.com>
2026-02-23 13:53:19 -0300

    fix: sitemap canonical www domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://guilhermerech.com.br",
+      url: "https://www.guilhermerech.com.br",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
